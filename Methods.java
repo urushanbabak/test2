bda23ca3e82b619ffc09507c5aa534d87b81afda
urushanbabak <100337073+urushanbabak@users.noreply.github.com>
2022-03-08 15:01:26 +0330

    start

---
+++ Methods.java
@@ -0,0 +1,14 @@
+scanner.hasnext();
+char.At();
+inputString.length;
+
+  /*
+                
+                System.out.println(Arrays.toString(new_twoNumbers));
+                System.out.println(Arrays.toString(rev_new_twoNumbers));
+                System.out.println(Arrays.toString(new_threeNumbers));
+                System.out.println(Arrays.toString(rev_new_threeNumbers));
+                System.out.println(Arrays.toString(new_fourNumbers));
+                System.out.println(Arrays.toString(rev_new_fourNumbers));
+                System.out.println(index);
+*/
